acc1f5fc1c841a8c0a6cc3277d6b5eb47090b3d5
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-04-02 22:58:39 +0700

    deploy setting

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={'/household-goods-store-sultan'}>
       <App/>
     </BrowserRouter>
   </Provider>
